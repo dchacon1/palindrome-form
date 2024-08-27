b0f81de1913dd09851caf6937644679c6a145d2e
Daniel Chacon <devdc.tech@gmail>
2024-08-27 11:44:31 -0700

    Adding PHP

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
+<?php
+    $title = "Palindrome";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Palindrome</title>
+    <title><?= $title ?></title>
 </head>
 <body>
-<form id="palindrome">
-    <h2>Is it a Palindrome?</h2>
-    <div class="form-field">
-        <label for="text-input">Enter text</label>
-        <input type="text" id="text-input" name="text-input">
-    </div>
+    <form id="palindrome">
+        <h2>Is it a Palindrome?</h2>
+        <div class="form-field">
+            <label for="text-input">Enter text</label>
+            <input type="text" id="text-input" name="text-input">
+        </div>
 
-    <button id="check-btn" type="submit">Check</button>
+        <button id="check-btn" type="submit">Check</button>
 
-    <p id="result" aria-live="polite"></p>
-</form>
+        <p id="result" aria-live="polite"></p>
+    </form>
 
-<script src="palindrome.js"></script>
+    <script src="palindrome.js"></script>
 </body>
 </html>
